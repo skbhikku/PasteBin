b704177e5e86348e9fc3daf427db0232c1181d54
skbhikku <147899420+skbhikku@users.noreply.github.com>
2025-12-30 20:27:14 +0530

    Use environment variable for backend URL

--- pastebin/src/api.js
+++ pastebin/src/api.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = import.meta.env.VITE_API_URL;
 
 export const createPaste = async (data) => {
   const res = await fetch(`${BACKEND_URL}/api/pastes`, {
